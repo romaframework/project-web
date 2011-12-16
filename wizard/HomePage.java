0686a90a3757db219f3f1040c66aab5b1ace0a1b
Tglman <tglman@tglman.com>
2011-12-16 20:44:38 +0100

    Refactor of flow aspect with new popup management.

--- wizard/HomePage.java
+++ wizard/HomePage.java
@@ -5,13 +5,12 @@ import org.romaframework.aspect.view.annotation.ViewClass;
 import org.romaframework.aspect.view.annotation.ViewField;
 import org.romaframework.frontend.domain.page.HomePageBasic;
 
-@ViewClass(layout = "screen://body")
 public class HomePage extends HomePageBasic {
 
 	public HomePage() {
 	}
 
-	@ViewField(render = ViewConstants.RENDER_URL, label = "", layout = "screen://header")
+	@ViewField(render = ViewConstants.RENDER_URL, label = "", layout = "header")
 	public String getHeader() {
 		return "${application}${session}/dynamic/common/header.jsp";
 	}
